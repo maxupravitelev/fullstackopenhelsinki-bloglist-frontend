ac359c087cb50ef0b66ceb9a73d737b30b90d37e
Max Upravitelev <max@upravitelev.de>
2020-09-16 16:53:55 +0200

    update handleLogin

--- src/App.js
+++ src/App.js
@@ -6,8 +6,10 @@ import loginService from './services/login'
 
 const App = () => {
   const [blogs, setBlogs] = useState([])
+  const [errorMessage, setErrorMessage] = useState(null)
   const [username, setUsername] = useState('') 
   const [password, setPassword] = useState('') 
+  const [user, setUser] = useState(null)
 
   useEffect(() => {
     blogService.getAll().then(blogs =>
@@ -15,11 +17,25 @@ const App = () => {
     )  
   }, [])
 
-  const handleLogin = (event) => {
+  const handleLogin = async (event) => {
     event.preventDefault()
-    console.log('logging in with', username, password)
+    try {
+      const user = await loginService.login({
+        username, password,
+      })
+
+      setUser(user)
+      setUsername('')
+      setPassword('')
+    } catch (exception) {
+      setErrorMessage('Wrong credentials')
+      setTimeout(() => {
+        setErrorMessage(null)
+      }, 5000)
+    }
   }
 
+  if (user === null) {
   return (
     <div>
       <h2>Login</h2>
@@ -44,6 +60,12 @@ const App = () => {
         </div>
         <button type="submit">login</button>
       </form>
+      </div>
+    )
+  }
+
+      return (
+    <div>
       <h2>blogs</h2>
       {blogs.map(blog =>
         <Blog key={blog.id} blog={blog} />
