ef159b51703e70e66a560a22b6436bd551246ca4
Max Upravitelev <max@upravitelev.de>
2020-09-17 11:40:02 +0200

    refactor login

--- src/App.js
+++ src/App.js
@@ -35,43 +35,108 @@ const App = () => {
     }
   }
 
-  if (user === null) {
+  const loginForm = () => (
+    <form onSubmit={handleLogin}>
+      <div>
+        username
+          <input
+          type="text"
+          value={username}
+          name="Username"
+          onChange={({ target }) => setUsername(target.value)}
+        />
+      </div>
+      <div>
+        password
+          <input
+          type="password"
+          value={password}
+          name="Password"
+          onChange={({ target }) => setPassword(target.value)}
+        />
+      </div>
+      <button type="submit">login</button>
+    </form>      
+  )
+
+  // const noteForm = () => (
+  //   <form onSubmit={addNote}>
+  //     <input
+  //       value={newNote}
+  //       onChange={handleNoteChange}
+  //     />
+  //     <button type="submit">save</button>
+  //   </form>  
+  // )
+
+
   return (
     <div>
-      <h2>Login</h2>
-      <form onSubmit={handleLogin}>
-        <div>
-          username
-            <input
-            type="text"
-            value={username}
-            name="Username"
-            onChange={({ target }) => setUsername(target.value)}
-          />
-        </div>
-        <div>
-          password
-            <input
-            type="password"
-            value={password}
-            name="Password"
-            onChange={({ target }) => setPassword(target.value)}
-          />
-        </div>
-        <button type="submit">login</button>
-      </form>
-      </div>
-    )
-  }
+      <h1>Login</h1>
 
-      return (
-    <div>
-      <h2>blogs</h2>
-      {blogs.map(blog =>
-        <Blog key={blog.id} blog={blog} />
-      )}
+      {/* <Notification message={errorMessage} /> */}
+
+      {user === null && loginForm()}
+      {/* {user !== null && noteForm()} */}
+
+      {/* <div>
+        <button onClick={() => setShowAll(!showAll)}>
+          show {showAll ? 'important' : 'all'}
+        </button>
+      </div> */}
+      {/* <ul>
+        {notesToShow.map((note, i) => 
+          <Note
+            key={i}
+            note={note} 
+            toggleImportance={() => toggleImportanceOf(note.id)}
+          />
+        )} */}
+      {/* </ul> */}
+
+      {/* <Footer /> */}
     </div>
   )
 }
+
+
+  // if (user === null) {
+  // return (
+  //   <div>
+  //     <h2>Login</h2>
+  //     <form onSubmit={handleLogin}>
+  //       <div>
+  //         username
+  //           <input
+  //           type="text"
+  //           value={username}
+  //           name="Username"
+  //           onChange={({ target }) => setUsername(target.value)}
+  //         />
+  //       </div>
+  //       <div>
+  //         password
+  //           <input
+  //           type="password"
+  //           value={password}
+  //           name="Password"
+  //           onChange={({ target }) => setPassword(target.value)}
+  //         />
+  //       </div>
+  //       <button type="submit">login</button>
+  //     </form>
+  //     </div>
+  //   )
+  // }
+
+  //     return (
+  //   <div>
+  //     <h2>blogs</h2>
+  //     {blogs.map(blog =>
+  //       <Blog key={blog.id} blog={blog} />
+  //     )}
+  //   </div>
+  // )
+//}
 
 export default App
