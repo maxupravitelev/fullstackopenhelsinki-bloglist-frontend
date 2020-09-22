7054132ab75b0327c63ce6eb6abc4f97f494be9d
Max Upravitelev <max@upravitelev.de>
2020-09-22 12:39:39 +0200

    add blogStyle to Blog.js component

--- src/components/Blog.js
+++ src/components/Blog.js
@@ -1,12 +1,23 @@
-import React from 'react'
-const Blog = ({ blog }) => (
-  <div>
-    {blog.title} {blog.author} 
-    <br></br>
-    {blog.url}
-    <br></br>
+import React from "react";
+const Blog = ({ blog }) => {
+  const blogStyle = {
+    paddingTop: 10,
+    paddingLeft: 2,
+    border: "solid",
+    borderWidth: 1,
+    marginBottom: 5,
+  };
 
-  </div>
-)
+  return (
+    <div style={blogStyle}>
+      <div>
+        {blog.title} {blog.author}
+        <br></br>
+        {blog.url}
+        <br></br>
+      </div>
+    </div>
+  );
+};
 
-export default Blog
+export default Blog;
