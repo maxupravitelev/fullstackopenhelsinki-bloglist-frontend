e1c52b285d268c6091fc5745e2c017ce59c13ceb
Max Upravitelev <max@upravitelev.de>
2020-09-22 13:16:49 +0200

    finish up exercise 5.7

--- src/components/Blog.js
+++ src/components/Blog.js
@@ -1,5 +1,8 @@
-import React from "react";
+import React, { useState } from "react";
+
 const Blog = ({ blog }) => {
+  const [blogExpanded, setBlogExpanded] = useState(false);
+
   const blogStyle = {
     paddingTop: 10,
     paddingLeft: 2,
@@ -8,16 +11,24 @@ const Blog = ({ blog }) => {
     marginBottom: 5,
   };
 
-  return (
-    <div style={blogStyle}>
-      <div>
+  if (blogExpanded === true) {
+    return (
+      <div style={blogStyle}>
         {blog.title} {blog.author}
         <br></br>
         {blog.url}
         <br></br>
+        <button onClick={() => setBlogExpanded(false)}>hide</button>
       </div>
-    </div>
-  );
+    );
+  } else {
+    return (
+      <div style={blogStyle}>
+        {blog.title}
+        <br></br>
+        <button onClick={() => setBlogExpanded(true)}>view</button>
+      </div>
+    );
+  }
 };
-
 export default Blog;
