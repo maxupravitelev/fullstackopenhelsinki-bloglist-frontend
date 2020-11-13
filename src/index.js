96da806de03303d1455b61500fdd11468a2ef798
Max Upravitelev <max@upravitelev.de>
2020-11-13 14:17:42 +0100

    init redux

--- src/index.js
+++ src/index.js
@@ -1,8 +1,16 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
+import { Provider } from 'react-redux'
 import App from './App'
+import store from './store'
 
-ReactDOM.render(<App />, document.getElementById('root'))
 
-// isntall dependecies for cypress:
+ReactDOM.render(
+  <Provider store={store} >
+    <App  />
+  </Provider>,
+  document.getElementById('root')
+)
+
+// install dependecies for cypress:
 // sudo apt-get install libgtk2.0-0 libgtk-3-0 libgbm-dev libnotify-dev libgconf-2-4 libnss3 libxss1 libasound2 libxtst6 xauth xvfb
